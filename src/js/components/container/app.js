aeee7dbd22985216235d7bac4c64c2c1fc53c5d8
Vis Viva <a.vis.viva@gmail.com>
2018-08-01 10:46:53 +0200

    Fixing babel

--- src/js/components/container/app.js
+++ src/js/components/container/app.js
@@ -1,5 +1,6 @@
-import React from "react";
-import {Component} from "react";
+import React, {
+    Component
+} from "react";
 
 import List from "./list";
 
@@ -8,18 +9,52 @@ class App extends Component {
         super();
         this.state = {
             b:10
-
         }
-        this.lan.bind(this);
+
+        /**
+         * Можно сделать так
+         * 
+         * this.hello = this.hello.bind(this);
+         */
     }
-    lan(){
-        this.setState({b: this.state.b + 1})
+
+    /**
+     * Если написать так
+     * 
+     * increment() {
+     *     this.setState({
+     *         b: this.state.b + 1
+     *     });
+     * }
+     */
+
+    /**
+     * Еще можно сделать так
+     */
+
+    increment = () => {
+        this.setState({
+            b: this.state.b + 1
+        });
     }
+
+    /**
+     * Эта стрелочная функция грубо говоря сохранена в самом объекте.
+     * На самом деле js так не разрешает - свойста на объекте класса можно создавать
+     * только в конструкторе, как например, мы создаем this.state = {...}
+     * 
+     * А можем мы сейчас так писать потому что я установил дополнительный плагин на бабель
+     * который называется transform-class-properties
+     * Проверь .babelrc
+     */
+    
     render() {
         return (
             <div>
-                <List a={this.state.b}
-                l = {this.lan}>
+                <List
+                    a={this.state.b}
+                    l={this.increment}
+                >
                     HHH
                 </List>
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+      javascript: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/',
+      filename: 'app.js'
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
         use: [
-          {
-            loader: "html-loader"
-          }
+            'babel-loader'
         ]
-      }
-    ]
+    }]
   },
   plugins: [
     new HtmlWebPackPlugin({
